a36a0af65eb83a6eb25dce096662b538d8f7fa0c
Luthfan Putra Prakarsa <luthfanputra.04@gmail.com>
2024-10-06 16:39:07 +0700

    add 3 operator aritmatika PBO

---
+++ JAVA - OOP/src/Praktikum/Sesi3/OperatorLogika.java
@@ -0,0 +1,21 @@
+package Praktikum.Sesi3;
+
+public class OperatorLogika {
+    public static void main(String[] args) {
+        int i = 0;
+        int j = 10;
+        boolean test = false;
+
+        // Demonstrasi &&
+        test = (i > 10) && (j++ > 9);
+        System.out.println(i);// 0
+        System.out.println(j);// 10
+        System.out.println(test);// false
+
+        // Demonstrasi &
+        test = (i > 10) & (j++ > 9);
+        System.out.println(i);// 0
+        System.out.println(j);// 11
+        System.out.println(test);// false
+    }
+}
---
+++ JAVA - OOP/src/Praktikum/Sesi3/OperatorRelasi.java
@@ -0,0 +1,23 @@
+package Praktikum.Sesi3;
+
+public class OperatorRelasi {
+    public static void main(String[] args) {
+        int i = 37;
+        int j = 42;
+        int k = 42;
+        System.out.println("Variable values...");
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+        System.out.println("k = " + k);
+
+        // Lebih besar dari
+        System.out.println("Greater than...");
+        System.out.println(" i > j = " + (i > j)); // false
+        System.out.println(" j > i = " + (j > i)); // true
+        System.out.println("k > j = " + (k > j)); // false
+
+        // lebih besar atau sama dengan
+        System.out.println("Greater than or equal to...");
+        System.out.println(" i >= j = " + (i >= j));
+    }
+}
